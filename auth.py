cad129f9e147de274a6353c575ca05d3eccab6a4
AleksandrZemcov <83689189+AleksandrZemcov@users.noreply.github.com>
2026-03-26 03:46:38 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,42 +1,20 @@
-def authenticate(login, password):
-    """Проверяет, является ли пользователь администратором."""
-    return login in admins and admins[login] == password
+def check_admin(login_input, password_input, admins_dict):
+    for login, password in admins_dict.items():
+        if login == login_input and password == password_input:
+            return True  # Пользователь является админом
+    return False # Пользователь не является админом
 
 
-def create_item():
-    title = input('Введите название авто')
-    price = int(input('Введите стоимость авто'))
-    id = get_max_id() + 1
-    new_item = {
-        'title': title,
-        'price': price,
-        'id': id
-    }
-    items.append(new_item)
-    print('Товар успешно добавлен в каталог!')
+admins = {
+    'admin1': 'pass123',
+    'admin2': 'qwerty'
+}
 
-def delete_item():
-    id_removed = int(input('Введите ID товара, который требуется удалить'))
-    for item in items:
-        if item['id'] == id_removed:
-            items.remove(item)
-            print('Товар успешно удален из каталога!')
-            for cart_item in cart:
-                if cart_item['id'] == id_removed:
-                    cart.remove(cart_item)
-                    print('Товар успешно удален из корзины!')
-            break
-    else:
-        print('Товар не найден!')
+# Использование функции
+user_login = input("Введите логин: ")
+user_password = input("Введите пароль: ")
 
-
-def change_item():
-    """Изменение стоимости товара"""
-    id_edit = int(input('Введите ID товара, для которого меняем цену\n'))
-    for item in items:
-        if item['id'] == id_edit:
-            item['price'] = int(input('Введите новую цену'))
-            print('Стоимость товара обновлена!')
-            break
-    else:
-        print("Вы ввели несуществующий ID")
+if check_admin(user_login, user_password, admins):
+    print("Доступ разрешен. Можно запускать методы управления товарами.")
+else:
+    print("Нет доступа.")
